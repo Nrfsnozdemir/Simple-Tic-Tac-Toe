c09cfc66ce7f19c84c95b8c347a371e0dbb10744
Nrfsnozdemir <avnurefsanozdemir@gmail.com>
2022-11-22 12:57:29 +0000

    Some tasks completed

--- Topics/The while and do-while loops/Squares of natural numbers/src/Main.java
+++ Topics/The while and do-while loops/Squares of natural numbers/src/Main.java
@@ -1,18 +1,31 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int input = scanner.nextInt();
+//
+//        for (int i = 1; i <= input; i++) {
+//            int result = i * i;
+//            if (result <= input) {
+//                System.out.println(result);
+//            } else {
+//                break;
+//            }
+//
+//        }
+
         Scanner scanner = new Scanner(System.in);
-        int input = scanner.nextInt();
 
-        for (int i = 1; i <= input; i++) {
-            int result = i * i;
-            if (result <= input) {
-                System.out.println(result);
-            } else {
-                break;
-            }
+        int len = scanner.nextInt(); // reading a length
+        int[] array = new int[len];  // creating an array with the specified length
 
+        for (int i = 0; i < len; i++) {
+            array[i] = scanner.nextInt(); // read the next number of the array
         }
+
+        System.out.println(Arrays.toString(array)); // output the array
+
     }
 }
